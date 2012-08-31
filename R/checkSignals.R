f4c32b3cda37c9837a388da01a09d1e52d705cfe
Thomas Cokelaer <cokelaer@ebi.ac.uk>
2012-08-31 16:32:10 +0000

    fix a typo.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CellNOptR@68989 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/checkSignals.R
+++ R/checkSignals.R
@@ -12,7 +12,7 @@
 #  CNO website: http://www.ebi.ac.uk/saezrodriguez/software.html
 #
 ##############################################################################
-# $Id: checkSignals.R 2267 2012-08-30 15:31:54Z cokelaer $
+# $Id: checkSignals.R 2287 2012-08-31 16:29:19Z cokelaer $
 checkSignals<-function(CNOlist, model ){
 
     if ((class(CNOlist)=="CNOlist")==FALSE){
@@ -50,7 +50,7 @@ checkSignals<-function(CNOlist, model ){
     if(any(signalsMatch == 0)){
         warning(paste(
             "The following inhibitors from your CNOlist do not match any of the species in your model, and should be removed:",
-            toString(colnames(CNOlisti@inhibitors)[which(signalsMatch == 0)])
+            toString(colnames(CNOlist@inhibitors)[which(signalsMatch == 0)])
             ))
     }
 
